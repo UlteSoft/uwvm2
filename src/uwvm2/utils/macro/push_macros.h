781dfceecb601ce5f6b86ffdac0f438245d71d82
MacroModel <liyn_xlfd@163.com>
2025-11-17 15:28:07 +0800

    Add support for ELF weak symbols in push_macros.h. Updated conditional compilation to clarify that weak symbol support is currently limited to ELF targets.

--- src/uwvm2/utils/macro/push_macros.h
+++ src/uwvm2/utils/macro/push_macros.h
@@ -581,6 +581,7 @@
 
 #pragma push_macro("UWVM_SUPPORT_WEAK_SYMBOL")
 #undef UWVM_SUPPORT_WEAK_SYMBOL
-#if UWVM_HAS_CPP_ATTRIBUTE(__gnu__::__weak__)
+// Currently only supports ELF weak symbols.
+#if defined(__ELF__) && UWVM_HAS_CPP_ATTRIBUTE(__gnu__::__weak__)
 # define UWVM_SUPPORT_WEAK_SYMBOL
 #endif
