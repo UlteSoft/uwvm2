dee24940b13c57727cdb00614e536fd88acc9296
MacroModel <liyn_xlfd@163.com>
2025-12-17 06:02:52 +0000

    Add fast_io_strict allocator implementation
    - Introduced a new allocator implementation under the `uwvm2::utils::allocator::fast_io_strict` namespace, providing various allocation strategies including standard, aligned, and zero-initialized allocations.
    - Implemented specific allocators for different environments, including Windows, Linux kernel, and mimalloc, enhancing flexibility and performance.
    - Added header and implementation files for each allocator type, ensuring modularity and maintainability of the allocation logic.
    - Included necessary attributes for non-null return guarantees where applicable, improving safety in memory management.

---
+++ src/uwvm2/utils/allocator/fast_io_strict/c_malloc.h
@@ -0,0 +1,103 @@
+﻿#pragma once
+
+namespace uwvm2::utils::allocator::fast_io_strict
+{
+    class fast_io_strict_c_malloc_allocator
+    {
+    public:
+#if __has_cpp_attribute(__gnu__::__returns_nonnull__)
+        [[__gnu__::__returns_nonnull__]]
+#endif
+        inline static void* allocate(::std::size_t n) noexcept
+        {
+            if(n == 0) { n = 1; }
+            void* p = ::std::malloc(n);
+            return p;
+        }
+#if __has_cpp_attribute(__gnu__::__returns_nonnull__)
+        [[__gnu__::__returns_nonnull__]]
+#endif
+        inline static void* reallocate(void* p, ::std::size_t n) noexcept
+        {
+            if(n == 0) { n = 1; }
+            ::std::size_t const to_allocate{n};
+            p = ::std::realloc(p, to_allocate);
+            return p;
+        }
+#if __has_cpp_attribute(__gnu__::__returns_nonnull__)
+        [[__gnu__::__returns_nonnull__]]
+#endif
+        inline static void* allocate_zero(::std::size_t n) noexcept
+        {
+            if(n == 0) { n = 1; }
+            void* p = ::std::calloc(1, n);
+            return p;
+        }
+#if (__has_include(<malloc.h>) || __has_include(<malloc_np.h>)) && !defined(__MSDOS__) && !defined(__LLVM_LIBC__)
+        inline static allocation_least_result allocate_at_least(::std::size_t n) noexcept
+        {
+            auto p{::fast_io::c_malloc_allocator::allocate(n)};
+            return {p, ::fast_io::details::c_malloc_usable_size_impl(p)};
+        }
+
+        inline static allocation_least_result allocate_zero_at_least(::std::size_t n) noexcept
+        {
+            auto p{::fast_io::c_malloc_allocator::allocate_zero(n)};
+            return {p, ::fast_io::details::c_malloc_usable_size_impl(p)};
+        }
+
+        inline static allocation_least_result reallocate_at_least(void* oldp, ::std::size_t n) noexcept
+        {
+            auto p{::fast_io::c_malloc_allocator::reallocate(oldp, n)};
+            return {p, ::fast_io::details::c_malloc_usable_size_impl(p)};
+        }
+#endif
+
+#if defined(_WIN32) && !defined(__WINE__) && !defined(__CYGWIN__)
+# if __has_cpp_attribute(__gnu__::__returns_nonnull__)
+        [[__gnu__::__returns_nonnull__]]
+# endif
+        inline static void* allocate_aligned(::std::size_t alignment, ::std::size_t n) noexcept
+        {
+            if(n == 0) { n = 1; }
+            void* p;
+            if(alignment <= __STDCPP_DEFAULT_NEW_ALIGNMENT__) { p = ::std::malloc(n); }
+            else
+            {
+                p = ::fast_io::noexcept_call(_aligned_malloc, n, alignment);
+            }
+            return p;
+        }
+# if __has_cpp_attribute(__gnu__::__returns_nonnull__)
+        [[__gnu__::__returns_nonnull__]]
+# endif
+        inline static void* reallocate_aligned(void* p, ::std::size_t alignment, ::std::size_t n) noexcept
+        {
+            if(n == 0) { n = 1; }
+            if(alignment <= __STDCPP_DEFAULT_NEW_ALIGNMENT__) { p = ::std::realloc(p, n); }
+            else
+            {
+                p = ::fast_io::noexcept_call(_aligned_realloc, p, n, alignment);
+            }
+            return p;
+        }
+
+        inline static void deallocate_aligned(void* p, ::std::size_t alignment) noexcept
+        {
+            if(p == nullptr) { return; }
+            if(alignment <= __STDCPP_DEFAULT_NEW_ALIGNMENT__) { ::std::free(p); }
+            else
+            {
+                ::fast_io::noexcept_call(_aligned_free, p);
+            }
+        }
+#endif
+        inline static void deallocate(void* p) noexcept
+        {
+            if(p == nullptr) { return; }
+
+            ::std::free(p);
+        }
+    };
+
+}  // namespace uwvm2::utils::allocator::fast_io_strict
---
+++ src/uwvm2/utils/allocator/fast_io_strict/impl.cppm
@@ -0,0 +1,35 @@
+/*************************************************************
+ * Ultimate WebAssembly Virtual Machine (Version 2)          *
+ * Copyright (c) 2025-present UlteSoft. All rights reserved. *
+ * Licensed under the APL-2.0 License (see LICENSE file).    *
+ *************************************************************/
+
+/**
+ * @author      MacroModel
+ * @version     2.0.0
+ * @date        2025-03-24
+ * @copyright   APL-2.0 License
+ */
+
+/****************************************
+ *  _   _ __        ____     __ __  __  *
+ * | | | |\ \      / /\ \   / /|  \/  | *
+ * | | | | \ \ /\ / /  \ \ / / | |\/| | *
+ * | |_| |  \ V  V /    \ V /  | |  | | *
+ *  \___/    \_/\_/      \_/   |_|  |_| *
+ *                                      *
+ ****************************************/
+
+module;
+
+#include "impl.h"
+
+export module uwvm2.utils.allocator.fast_io_strict;
+
+export namespace uwvm2::utils::allocator::fast_io_strict
+{
+    using ::uwvm2::utils::allocator::fast_io_strict::native_strict_global_allocator;
+    using ::uwvm2::utils::allocator::fast_io_strict::native_strict_thread_local_allocator;
+    using ::uwvm2::utils::allocator::fast_io_strict::native_strict_typed_global_allocator;
+    using ::uwvm2::utils::allocator::fast_io_strict::native_strict_typed_thread_local_allocator;
+}  // namespace uwvm2::utils::allocator::fast_io_strict
