a2acd63baf1f90e62f184139bdaf66b4ceb3f2d9
MacroModel <liyn_xlfd@163.com>
2025-12-21 17:47:18 +0800

    Add local_imported module support in WASM storage implementation
    - Included `local_imported` in both `impl.cppm` and `impl.h` to enhance the WASM storage capabilities.
    - This addition facilitates better handling of local imports, aligning with recent enhancements in local import features.

--- src/uwvm2/uwvm/wasm/storage/local_imported.cppm
+++ src/uwvm2/uwvm/wasm/storage/local_imported.cppm
@@ -1,10 +1,12 @@
-﻿/*************************************************************
+/*************************************************************
  * Ultimate WebAssembly Virtual Machine (Version 2)          *
  * Copyright (c) 2025-present UlteSoft. All rights reserved. *
  * Licensed under the APL-2.0 License (see LICENSE file).    *
  *************************************************************/
 
 /**
+ * @brief       Imported local dynamic libraries
+ * @details     "--wasm-load-dl" or "-Wld"
  * @author      MacroModel
  * @version     2.0.0
  * @date        2025-03-28
@@ -22,15 +24,18 @@
 
 module;
 
-export module uwvm2.uwvm.wasm.storage;
-export import :mode;
-export import :execute_wasm;
-export import :preloaded_wasm;
-export import :local_preload;
-export import :local_imported;
-export import :preloaded_dl;
-export import :weak_symbol;
-export import :all_module;
+// macro
+#include <uwvm2/utils/macro/push_macros.h>
+
+export module uwvm2.uwvm.wasm.storage:local_imported;
+
+import fast_io;
+import uwvm2.utils.container;
+import uwvm2.parser.wasm.concepts;
+import uwvm2.parser.wasm.standard.wasm1.type;
+import uwvm2.uwvm.wasm.base;
+import uwvm2.uwvm.wasm.feature;
+import uwvm2.uwvm.wasm.type;
 
 #ifndef UWVM_MODULE
 # define UWVM_MODULE
@@ -39,4 +44,4 @@ export import :all_module;
 # define UWVM_MODULE_EXPORT export
 #endif
 
-#include "impl.h"
+#include "local_imported.h"
---
+++ src/uwvm2/uwvm/wasm/storage/local_imported.h
@@ -0,0 +1,52 @@
+/*************************************************************
+ * Ultimate WebAssembly Virtual Machine (Version 2)          *
+ * Copyright (c) 2025-present UlteSoft. All rights reserved. *
+ * Licensed under the APL-2.0 License (see LICENSE file).    *
+ *************************************************************/
+
+/**
+ * @brief       Imported local dynamic libraries
+ * @details     "--wasm-load-dl" or "-Wld"
+ * @author      MacroModel
+ * @version     2.0.0
+ * @date        2025-03-28
+ * @copyright   APL-2.0 License
+ */
+
+/****************************************
+ *  _   _ __        ____     __ __  __  *
+ * | | | |\ \      / /\ \   / /|  \/  | *
+ * | | | | \ \ /\ / /  \ \ / / | |\/| | *
+ * | |_| |  \ V  V /    \ V /  | |  | | *
+ *  \___/    \_/\_/      \_/   |_|  |_| *
+ *                                      *
+ ****************************************/
+
+#pragma once
+
+#ifndef UWVM_MODULE
+// macro
+# include <uwvm2/utils/macro/push_macros.h>
+// import
+# include <fast_io.h>
+# include <uwvm2/utils/container/impl.h>
+# include <uwvm2/parser/wasm/concepts/impl.h>
+# include <uwvm2/parser/wasm/standard/wasm1/type/impl.h>
+# include <uwvm2/uwvm/wasm/base/impl.h>
+# include <uwvm2/uwvm/wasm/feature/impl.h>
+# include <uwvm2/uwvm/wasm/type/impl.h>
+#endif
+
+#ifndef UWVM_MODULE_EXPORT
+# define UWVM_MODULE_EXPORT
+#endif
+
+UWVM_MODULE_EXPORT namespace uwvm2::uwvm::wasm::storage
+{
+    inline ::uwvm2::utils::container::vector<::uwvm2::uwvm::wasm::type::local_imported_t> preload_local_imported{};  //[global]
+}  // namespace uwvm2::uwvm::wasm::storage
+
+#ifndef UWVM_MODULE
+// macro
+# include <uwvm2/utils/macro/pop_macros.h>
+#endif
