91bce00557dbe16a1e013bd31aac48c50b23aa31
MacroModel <liyn_xlfd@163.com>
2025-12-22 09:00:33 +0800

    Add initial implementation files for WASI Preview 1
    - Introduced new module files for `wasip1` including `impl.cppm`, `impl.h`, `define.cppm`, `define.h`, and local imported implementations.
    - Established module exports and included necessary headers to support WASI functionality.
    - Enhanced code organization and documentation for better maintainability and clarity in the WASI environment integration.

---
+++ src/uwvm2/uwvm/imported/wasi/wasip1/local_imported/define.h
@@ -0,0 +1,69 @@
+﻿/*************************************************************
+ * Ultimate WebAssembly Virtual Machine (Version 2)          *
+ * Copyright (c) 2025-present UlteSoft. All rights reserved. *
+ * Licensed under the APL-2.0 License (see LICENSE file).    *
+ *************************************************************/
+
+/**
+ * @author      MacroModel
+ * @version     2.0.0
+ * @date        2025-03-27
+ * @copyright   APL-2.0 License
+ */
+
+/****************************************
+ *  _   _ __        ____     __ __  __  *
+ * | | | |\ \      / /\ \   / /|  \/  | *
+ * | | | | \ \ /\ / /  \ \ / / | |\/| | *
+ * | |_| |  \ V  V /    \ V /  | |  | | *
+ *  \___/    \_/\_/      \_/   |_|  |_| *
+ *                                      *
+ ****************************************/
+
+#pragma once
+
+#ifndef UWVM_MODULE
+// std
+# include <cstddef>
+# include <cstdint>
+# include <type_traits>
+# include <utility>
+// macro
+# include <uwvm2/utils/macro/push_macros.h>
+# include <uwvm2/uwvm/utils/ansies/uwvm_color_push_macro.h>  // wasi
+# ifndef UWVM_DISABLE_LOCAL_IMPORTED_WASIP1
+#  include <uwvm2/imported/wasi/wasip1/feature/feature_push_macro.h>  // wasip1
+# endif
+// import
+# include <fast_io.h>
+# include <uwvm2/utils/ansies/impl.h>
+# include <uwvm2/utils/debug/impl.h>
+# include <uwvm2/imported/wasi/wasip1/impl.h>
+# include <uwvm2/uwvm/wasm/type/impl.h>
+#endif
+
+#ifndef UWVM_MODULE_EXPORT
+# define UWVM_MODULE_EXPORT
+#endif
+
+UWVM_MODULE_EXPORT namespace uwvm2::uwvm::imported::wasi::wasip1::local_imported
+{
+#ifndef UWVM_DISABLE_LOCAL_IMPORTED_WASIP1
+# if defined(UWVM_IMPORT_WASI_WASIP1)
+
+/// @todo Wrap all function definitions in wasip1 into a local_imported module.
+/// @note struct wasip1_local_imported_module_t{}; // meet concept ::uwvm2::uwvm::wasm::type::is_local_imported_module
+
+
+# endif
+#endif
+}  // namespace uwvm2::uwvm::imported::wasi::wasip1::storage
+
+#ifndef UWVM_MODULE
+// macro
+# ifndef UWVM_DISABLE_LOCAL_IMPORTED_WASIP1
+#  include <uwvm2/imported/wasi/wasip1/feature/feature_pop_macro.h>  // wasip1
+# endif
+# include <uwvm2/uwvm/utils/ansies/uwvm_color_pop_macro.h>  // wasi
+# include <uwvm2/utils/macro/pop_macros.h>
+#endif
