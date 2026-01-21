5738421941ca6d9063d09028d1406ca50bc3e41c
MacroModel <liyn_xlfd@163.com>
2026-01-21 23:40:13 +0800

    Refactor floating-point handling in Wasm compiler's optable
    - Updated ARM NEON conditional compilation directives for clarity and compatibility.
    - Enhanced lane access methods for floating-point types using `front_unchecked()` for improved performance.
    - Streamlined bit casting for floating-point conversions, enhancing code readability and maintainability.

--- src/uwvm2/runtime/compiler/uwvm_int/optable/define.h
+++ src/uwvm2/runtime/compiler/uwvm_int/optable/define.h
@@ -94,7 +94,7 @@ UWVM_MODULE_EXPORT namespace uwvm2::runtime::compiler::uwvm_int::optable
 
     namespace details
     {
-#if defined(__ARM_NEON)
+#if defined(__ARM_NEON) & UWVM_INTERPRETER_FORCE_USE_ARM_NEON_TO_SPLIT_V128
 # if defined(__clang__)
         using float64x1_t = ::uwvm2::parser::wasm::standard::wasm1::type::wasm_f64 [[clang::neon_vector_type(1)]];
         using float64x2_t = ::uwvm2::parser::wasm::standard::wasm1::type::wasm_f64 [[clang::neon_vector_type(2)]];
@@ -118,7 +118,7 @@ UWVM_MODULE_EXPORT namespace uwvm2::runtime::compiler::uwvm_int::optable
         UWVM_ALWAYS_INLINE inline constexpr ::uwvm2::parser::wasm::standard::wasm1::type::wasm_f32
             get_f32_low_from_f64_slot(::uwvm2::parser::wasm::standard::wasm1::type::wasm_f64 v) noexcept
         {
-#if defined(__ARM_NEON)
+#if defined(__ARM_NEON) & UWVM_INTERPRETER_FORCE_USE_ARM_NEON_TO_SPLIT_V128
 # if UWVM_HAS_BUILTIN(__builtin_neon_vget_lane_f32) && UWVM_HAS_BUILTIN(__builtin_shufflevector)  // clang
             float32x2_t s2{::std::bit_cast<float32x2_t>(v)};
             if constexpr(::std::endian::native == ::std::endian::big)
@@ -144,16 +144,15 @@ UWVM_MODULE_EXPORT namespace uwvm2::runtime::compiler::uwvm_int::optable
 # endif
 #else
             // x86_64, ...
-            auto const u64{::std::bit_cast<::std::uint_least64_t>(v)};
-            auto const u32{static_cast<::std::uint_least32_t>(u64)};
-            return ::std::bit_cast<::uwvm2::parser::wasm::standard::wasm1::type::wasm_f32>(u32);
+            auto const f32x2{::std::bit_cast<::uwvm2::utils::container::array<::uwvm2::parser::wasm::standard::wasm1::type::wasm_f32, 2uz>>(v)};
+            return f32x2.front_unchecked();
 #endif
         }
 
         UWVM_ALWAYS_INLINE inline constexpr ::uwvm2::parser::wasm::standard::wasm1::type::wasm_f32
             get_f32_low_from_v128_slot(::uwvm2::parser::wasm::standard::wasm1p1::type::wasm_v128 v) noexcept
         {
-#if defined(__ARM_NEON)
+#if defined(__ARM_NEON) & UWVM_INTERPRETER_FORCE_USE_ARM_NEON_TO_SPLIT_V128
 # if UWVM_HAS_BUILTIN(__builtin_neon_vgetq_lane_f32) && UWVM_HAS_BUILTIN(__builtin_shufflevector)  // clang
             float32x4_t s4{::std::bit_cast<float32x4_t>(v)};
             if constexpr(::std::endian::native == ::std::endian::big)
@@ -190,15 +189,15 @@ UWVM_MODULE_EXPORT namespace uwvm2::runtime::compiler::uwvm_int::optable
 # endif
 #else
             // x86_64, ...
-            auto const u32x4{::std::bit_cast<::uwvm2::utils::container::array<::std::uint_least32_t, 4uz>>(v)};
-            return ::std::bit_cast<::uwvm2::parser::wasm::standard::wasm1::type::wasm_f32>(u32x4.front_unchecked());
+            auto const f32x4{::std::bit_cast<::uwvm2::utils::container::array<::uwvm2::parser::wasm::standard::wasm1::type::wasm_f32, 4uz>>(v)};
+            return f32x4.front_unchecked();
 #endif
         }
 
         UWVM_ALWAYS_INLINE inline constexpr ::uwvm2::parser::wasm::standard::wasm1::type::wasm_f64
             get_f64_low_from_v128_slot(::uwvm2::parser::wasm::standard::wasm1p1::type::wasm_v128 v) noexcept
         {
-#if defined(__ARM_NEON)
+#if defined(__ARM_NEON) & UWVM_INTERPRETER_FORCE_USE_ARM_NEON_TO_SPLIT_V128
 # if UWVM_HAS_BUILTIN(__builtin_neon_vgetq_lane_f64) && UWVM_HAS_BUILTIN(__builtin_shufflevector)  // clang
             float64x2_t d2{::std::bit_cast<float64x2_t>(v)};
             if constexpr(::std::endian::native == ::std::endian::big)
@@ -232,8 +231,8 @@ UWVM_MODULE_EXPORT namespace uwvm2::runtime::compiler::uwvm_int::optable
 # endif
 #else
             // x86_64, ...
-            auto const u64x2{::std::bit_cast<::uwvm2::utils::container::array<::std::uint_least64_t, 2uz>>(v)};
-            return ::std::bit_cast<::uwvm2::parser::wasm::standard::wasm1::type::wasm_f64>(u64x2.front_unchecked());
+            auto const f64x2{::std::bit_cast<::uwvm2::utils::container::array<::uwvm2::parser::wasm::standard::wasm1::type::wasm_f64, 2uz>>(v)};
+            return f64x2.front_unchecked();
 #endif
         }
     }  // namespace details
