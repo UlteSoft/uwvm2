4467d5e625c8096c76de4ba1785095aec25e8f3f
MacroModel <liyn_xlfd@163.com>
2026-02-08 18:55:16 +0800

    Update macro definitions and enhance conditional compilation for x86_64 ABI
    - Modified the definition of `UWVM_INTERPRETER_OPFUNC_TYPE_MACRO` to include specific conditions for Windows x86_64 architecture, ensuring compatibility with both MS ABI and SysV ABI.
    - Improved the conditional compilation logic in `uwvm_runtime.default.cpp` to better handle stack-top register positions for x86_64, enhancing performance and clarity in the codebase.

--- src/uwvm2/runtime/compiler/uwvm_int/macro/push_macros.h
+++ src/uwvm2/runtime/compiler/uwvm_int/macro/push_macros.h
@@ -28,7 +28,11 @@
 // macro checkers
 #pragma push_macro("UWVM_INTERPRETER_OPFUNC_TYPE_MACRO")
 #undef UWVM_INTERPRETER_OPFUNC_TYPE_MACRO
-#define UWVM_INTERPRETER_OPFUNC_TYPE_MACRO
+#if defined(_WIN32) && (defined(__x86_64__) || defined(_M_AMD64)) && (defined(__GNUC__) || defined(__clang__))
+# define UWVM_INTERPRETER_OPFUNC_TYPE_MACRO [[__gun__::__sysv_abi__]]
+#else
+# define UWVM_INTERPRETER_OPFUNC_TYPE_MACRO
+#endif
 
 #pragma push_macro("UWVM_INTERPRETER_OPFUNC_MACRO")
 #undef UWVM_INTERPRETER_OPFUNC_MACRO
--- src/uwvm2/runtime/lib/uwvm_runtime.default.cpp
+++ src/uwvm2/runtime/lib/uwvm_runtime.default.cpp
@@ -852,8 +852,10 @@ namespace uwvm2::runtime::uwvm_int
             res.i32_stack_top_end_pos = res.i64_stack_top_end_pos = 8uz;
             res.f32_stack_top_begin_pos = res.f64_stack_top_begin_pos = res.v128_stack_top_begin_pos = 8uz;
             res.f32_stack_top_end_pos = res.f64_stack_top_end_pos = res.v128_stack_top_end_pos = 16uz;
-#elif (defined(__x86_64__) || defined(_M_AMD64)) && !defined(_WIN32)
+#elif (defined(__x86_64__) || defined(_M_AMD64)) &&                                                                                                            \
+    ((!defined(_WIN32)) || (defined(_WIN32) && (defined(__x86_64__) || defined(_M_AMD64)) && (defined(__GNUC__) || defined(__clang__))))
             // x86_64: sysv abi
+            // x86_64: sysv abi in ms abi
             res.i32_stack_top_begin_pos = res.i64_stack_top_begin_pos = 3uz;
             res.i32_stack_top_end_pos = res.i64_stack_top_end_pos = 6uz;
             res.f32_stack_top_begin_pos = res.f64_stack_top_begin_pos = res.v128_stack_top_begin_pos = 6uz;
