d069b994665e35667769b13a6930311416f0939d
MacroModel <liyn_xlfd@163.com>
2026-06-16 20:08:06 +0800

    Enhance runtime LLVM JIT cache path description to clarify usage options. Updated the command-line parameter description to include the ability to disable the LLVM JIT cache directory, improving user guidance on cache management.

--- src/uwvm2/uwvm/cmdline/params/runtime_llvm_jit_cache_path.h
+++ src/uwvm2/uwvm/cmdline/params/runtime_llvm_jit_cache_path.h
@@ -62,7 +62,7 @@ UWVM_MODULE_EXPORT namespace uwvm2::uwvm::cmdline::params
 # endif
     inline constexpr ::uwvm2::utils::cmdline::parameter runtime_llvm_jit_cache_path{
         .name{u8"--runtime-llvm-jit-cache-path"},
-        .describe{u8"Select the runtime LLVM JIT cache directory mode."},
+        .describe{u8"Select the runtime LLVM JIT cache directory mode or disable it."},
         .usage{u8"[disable|default|path <path>]"},
         .alias{::uwvm2::utils::cmdline::kns_u8_str_scatter_t{::std::addressof(details::runtime_llvm_jit_cache_path_alias), 1uz}},
         .handle{::std::addressof(details::runtime_llvm_jit_cache_path_callback)},
